a1635a4de0e3a38f77f7badd9bfbc37eacc1efbe
371126627@qq.com <371126627@qq.com>
2016-06-22 18:33:03 +0800

    ok

--- Vote/Controllers/HomeController.cs
+++ Vote/Controllers/HomeController.cs
@@ -35,21 +35,34 @@ namespace Vote.Controllers
         [HttpGet]
         public ActionResult list(string vscode)
         {
+           
             using (IDatabase bs = VoteDB.GetInstance())
             {
+                Vote_session vs = bs.FirstOrDefault<Vote_session>(Sql.Builder.Where("VState=1"));
+                if (vs == null)
+                {
+                    ViewBag.mag = "alert('投票已经结束或尚未开始!')";
+                    return RedirectToAction("voteorder");
+                }
                 List<UserInfo> list = bs.Fetch<UserInfo>(Sql.Builder.Where(" vsid= (select top 1 id from Vote_session where VState=1)"));
-                Vote_User vuser = bs.FirstOrDefault<Vote_User>(Sql.Builder.Where("vscode=@0 and VsID=(select top 1 id from Vote_session where VState=1)", vscode));
-                if (vuser != null)
+                List<Vote_User> vusers = bs.Fetch<Vote_User>(Sql.Builder.Where("vscode=@0 and VsID=(select top 1 id from Vote_session where VState=1)", vscode));
+                if (vusers.Count>0)
                 {
                     Sql sql = new Sql();
                     sql.Append(@"SELECT a.*,b.SumVote FROM UserInfo a
-                 LEFT JOIN (SELECT UserID,COUNT(UserID) SumVote FROM Vote_user GROUP BY UserID)b ON a.ID=b.UserID
+                 LEFT JOIN (SELECT UserID,SUM(SumVote) SumVote FROM (                
+ SELECT b.UserID,CASE WHEN Vscode LIKE 'a%' THEN SumVote*3 ELSE SumVote END SumVote  FROM (                 
+ SELECT UserID,Vscode, COUNT(*) SumVote FROM Vote_user GROUP BY UserID,Vscode) b) c GROUP BY c.UserID)b ON a.ID=b.UserID
                   WHERE  a.VSID=(select top 1 id from Vote_session where VState=1)");
 
                      list = bs.Fetch<UserInfo>(sql);
                      foreach (var item in list)
                      {
                          item.Numer = "-2";
+                     }
+                     foreach (var item in vusers)
+                     {
+                         list.Find(n => n.ID == item.UserID).UserDese = "(已投)";
                      }
                 }
                 else
@@ -59,7 +72,7 @@ namespace Vote.Controllers
                         item.Numer = "-1";
                     }
                 }
-                Vote_session vs = bs.FirstOrDefault<Vote_session>(Sql.Builder.Where("VState=1"));
+               
                 ViewBag.list=list;
                 ViewBag.info = vs;
                 ViewBag.vscode = vscode;
@@ -72,16 +85,21 @@ namespace Vote.Controllers
             using (IDatabase bs = VoteDB.GetInstance())
             {
                 Vote_session vs = bs.FirstOrDefault<Vote_session>(Sql.Builder.Append("select top 1 * from Vote_session where VState=1"));
-                
+                if (vs == null)
+                {
+                    ViewBag.mag = "alert('投票已经结束或尚未开始!')";
+                    return RedirectToAction("voteorder");
+                }
                 VsCode code = bs.FirstOrDefault<VsCode>(Sql.Builder.Where("vscode=@0", vscode));
                 if (code == null)
                 {
                     ViewBag.mag = "alert('投票码输入错误!')";
                     return RedirectToAction("Index", new { vscode = vscode });
                 }
-                Vote_User vuser = bs.FirstOrDefault<Vote_User>(Sql.Builder.Where("vscode=@0 and VsID=(select top 1 id from Vote_session where VState=1)", vscode)); ;
-                if (vuser!=null)
+                List<Vote_User> vusers = bs.Fetch<Vote_User>(Sql.Builder.Where("vscode=@0 and VsID=(select top 1 id from Vote_session where VState=1)", vscode));
+                if (vusers.Count>0)
                 { 
+
                     //ViewBag.mag = "alert('投票码已经投过当前场次!')";
                 }
                 else
@@ -98,20 +116,28 @@ namespace Vote.Controllers
                     vuserinfo1.UserID = back;
                     vuserinfo1.CreateDate = DateTime.Now;
                     vuserinfo1.Vscode = vscode;
-                    vuserinfo.VsID = vs.ID;
+                    vuserinfo1.VsID = vs.ID;
                     //投票
                     bs.Insert<Vote_User>(vuserinfo1);
+                    vusers = bs.Fetch<Vote_User>(Sql.Builder.Where("vscode=@0 and VsID=(select top 1 id from Vote_session where VState=1)", vscode));
+                   // return RedirectToAction("list", new { vscode=vscode });
                     ViewBag.mag = "alert('投票成功!')";
                 }
                Sql sql=new Sql();
                 sql.Append(@"SELECT a.*,b.SumVote FROM UserInfo a
-                 LEFT JOIN (SELECT UserID,COUNT(UserID) SumVote FROM Vote_user GROUP BY UserID)b ON a.ID=b.UserID
+                 LEFT JOIN (SELECT UserID,SUM(SumVote) SumVote FROM (                
+ SELECT b.UserID,CASE WHEN Vscode LIKE 'a%' THEN SumVote*3 ELSE SumVote END SumVote  FROM (                 
+ SELECT UserID,Vscode, COUNT(*) SumVote FROM Vote_user GROUP BY UserID,Vscode) b) c GROUP BY c.UserID)b ON a.ID=b.UserID
                   WHERE  a.VSID=(select top 1 id from Vote_session where VState=1)");
                
                 List<UserInfo> list = bs.Fetch<UserInfo>(sql);
                 foreach (var item in list)
                 {
                     item.Numer = "-2";
+                }
+                foreach (var item in vusers)
+                {
+                    list.Find(n => n.ID == item.UserID).UserDese = "(已投)"; 
                 }
                 ViewBag.list = list;
                 ViewBag.info = vs;
@@ -122,10 +148,19 @@ namespace Vote.Controllers
         public ActionResult voteorder() {
             using (IDatabase bs = VoteDB.GetInstance())
             {
+                Vote_session vs = bs.FirstOrDefault<Vote_session>(Sql.Builder.Append("select top 1 * from Vote_session where VState=1"));
+                if (vs != null)
+                {
+                  
+                    return RedirectToAction("index");
+                }
                 Sql sql = new Sql();
                 sql.Append(@"SELECT a.*,b.SumVote FROM UserInfo a
-                 LEFT JOIN (SELECT UserID,COUNT(UserID) SumVote FROM Vote_user GROUP BY UserID)b ON a.ID=b.UserID
+                 LEFT JOIN (SELECT UserID,SUM(SumVote) SumVote FROM (                
+ SELECT b.UserID,CASE WHEN Vscode LIKE 'a%' THEN SumVote*3 ELSE SumVote END SumVote  FROM (                 
+ SELECT UserID,Vscode, COUNT(*) SumVote FROM Vote_user GROUP BY UserID,Vscode) b) c GROUP BY c.UserID)b ON a.ID=b.UserID
                   ");
+                ViewBag.mag = "alert('投票已经结束或尚未开始!')";
                 ViewBag.list = bs.Fetch<UserInfo>(sql).OrderByDescending(n=>n.SumVote).ToList();
             }
             return View();
@@ -139,7 +174,6 @@ namespace Vote.Controllers
                 //ViewBag.list = vs;
                 return View(vs);
             }
-           
         }
         public ActionResult SetStart(int id)
         {
@@ -149,7 +183,6 @@ namespace Vote.Controllers
                 {
                     bs.Update<Vote_session>(new Sql().Append("set VState=0 where VState=1"));
                     bs.Update<Vote_session>(new Sql().Append("set VState=1,StartDate=getDate(),endsDate=null where id=@0", id));
-                  
                 }
             }
             catch (Exception e)
@@ -165,7 +198,6 @@ namespace Vote.Controllers
                 using (IDatabase bs = VoteDB.GetInstance())
                 {
                     bs.Update<Vote_session>(new Sql().Append("set VState=0,endsDate=getDate()  where id=@0", id));
-                  
                 }
             }
             catch (Exception e)
@@ -181,7 +213,6 @@ namespace Vote.Controllers
         //        using (IDatabase bs = VoteDB.GetInstance())
         //        {
         //            bs.Update<Vote_session>(new Sql().Append("set VState=0,endsDate=getDate()  where id=@0", id));
-
         //        }
         //    }
         //    catch (Exception e)
@@ -191,7 +222,30 @@ namespace Vote.Controllers
         //    return RedirectToAction("vs");
         //}
         public ActionResult Showinfo() {
+
             return View();
+        }
+        public ActionResult GetVsInfo(int vsid)
+        {
+             using (IDatabase bs = VoteDB.GetInstance())
+             {
+                 Vote_session vs = bs.FirstOrDefault<Vote_session>(Sql.Builder.Where("ID=@0",vsid));
+                 Sql sql = new Sql();
+                 sql.Append(@"SELECT a.*,b.SumVote FROM UserInfo a
+                 LEFT JOIN (SELECT UserID,SUM(SumVote) SumVote FROM (                
+ SELECT b.UserID,CASE WHEN Vscode LIKE 'a%' THEN SumVote*3 ELSE SumVote END SumVote  FROM (                 
+ SELECT UserID,Vscode, COUNT(*) SumVote FROM Vote_user GROUP BY UserID,Vscode) b) c GROUP BY c.UserID)b ON a.ID=b.UserID
+                  ").Where("a.vsid=@0",vsid);
+                 List<UserInfo> listuser = bs.Fetch<UserInfo>(sql).OrderByDescending(n => n.SumVote).ToList();
+
+                 int c = bs.Single<int>(Sql.Builder.Append("SELECT COUNT(*) CO FROM (SELECT Vscode,COUNT(*) aa FROM Vote_User WHERE VsID=@0 GROUP BY Vscode)t", vsid));
+                 return Json(new { 
+                     info = vs, 
+                     arruser = listuser.Select(n => n.UserName), 
+                     arrcount = listuser.Select(n => n.SumVote),
+                     count = c
+                 });
+             }           
         }
     }
 }
